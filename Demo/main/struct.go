caa341a4ef15b7bdefc06cc1d2d3ebff82e06050
Suraj Shete <62868747+surajshete@users.noreply.github.com>
2023-01-30 07:23:00 +0000

    Struct code added

--- Demo/main/struct.go
+++ Demo/main/struct.go
@@ -11,24 +11,37 @@ func main() {
 	suraj := Ninja{name: "suraj", weapon: []string{"ninja star", "ninja kunai"}, level: 1}
 	fmt.Print(suraj)
 	vijay := Ninja{"vijay", []string{"star"}, 2}
-vijay.level++;
+	vijay.level++
 	fmt.Println(vijay)
 	type Dojo struct {
 		ninja Ninja
 		role  string
 	}
-	type DojoByReffrence struct{
+	type DojoByReffrence struct {
 		ninja *Ninja
-		name string
+		name  string
 	}
-	Dojo_ninja := Dojo{suraj, "HOD"} //pass by value
+	Dojo_ninja := Dojo{suraj, "HOD"}              //pass by value
 	Dojo_ninja2 := DojoByReffrence{&suraj, "HOD"} //pass by value
 	fmt.Println(Dojo_ninja)
-	fmt.Println(Dojo_ninja2.ninja) //it will give values with &
-	fmt.Println(*Dojo_ninja2.ninja)//it will give actual values
+	fmt.Println(Dojo_ninja2.ninja)  //it will give values with &
+	fmt.Println(*Dojo_ninja2.ninja) //it will give actual values
 	fmt.Println(Dojo_ninja2.ninja.name)
 
-	ninjaPointer :=new(Ninja)
+	ninjaPointer := new(Ninja)
 	fmt.Println(ninjaPointer)
 	fmt.Println(*ninjaPointer)
+}
+
+type NinjaIntern struct {
+	name  string
+	level int
+}
+
+func (NinjaIntern) sayHello() {
+	fmt.Printf("Hello..")
+}
+
+func (NinjaIntern) sayNae() {
+	fmt.Println("My name is " + NinjaIntern.name)
 }
