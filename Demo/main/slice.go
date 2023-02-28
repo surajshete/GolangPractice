c5ae6c9af396111f8c9b498939bffce6f7047b58
Suraj Shete <62868747+surajshete@users.noreply.github.com>
2023-02-28 09:19:11 +0000

    modified:   Demo/main/slice.go

--- Demo/main/slice.go
+++ Demo/main/slice.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	array_fixed_size := [...]int{1, 2, 3}//array
-	fmt.Println(array_fixed_size)
-	 //array_fixed_size=[...]int{1,2}//------------ it will
+	array_fixed_size := [...]int{1, 2, 3} //array
+	fmt.Println("array_fixed_size :", array_fixed_size)
+	//array_fixed_size=[...]int{1,2}//------------ it will
 	// when we declare array with fixed size we cannot reassign same array with diffrent size in that case we use slice\
 	//slice in go is similar toarraylist
-	sliceA := []int{1, 3, 4}//slice
+	sliceA := []int{1, 3, 4} //slice
 	fmt.Println(len(sliceA))
 	sliceA = []int{1, 4, 4, 4, 4}
-	fmt.Println(sliceA) 
+	fmt.Println(sliceA)
 }
